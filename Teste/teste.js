adacedcfa71a706d3cd6890914ea5d14a4fe0659
Dudu <edudocross@gmail.com>
2023-03-18 16:58:59 -0300

    new tests

---
+++ Teste/teste.js
@@ -0,0 +1,11 @@
+function teste(){
+let nome = document.getElementById("nome");
+let senha = document.getElementById("senha");
+
+if (nome.value == "Eduardo"){
+    console.log("Nome OK");
+} else {
+    console.log("Nome NÃO OK");
+}
+}
+
