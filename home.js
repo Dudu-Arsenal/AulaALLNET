70a5d0523782e294b74f7cbc4446e0aac62f4712
Dudu <edudocross@gmail.com>
2023-03-25 18:12:13 -0300

    login verify

--- home.js
+++ home.js
@@ -31,3 +31,19 @@ function setCor(){
 let mudaCor = document.querySelector('.nomeClasse');
 mudaCor.setAttribute('style', 'background-color: greenyellow;')
 } 
+
+function verificaUser(){
+    let nome = document.getElementById('nome');
+    let senha = document.getElementById('senha');
+    let incorreto = document.getElementById('incorreto');
+
+    if (nome.value == 'Edu' && senha.value == '123'){
+        incorreto.innerHTML = 'Logado com sucesso, você será redirecionado em instantes.'
+        incorreto.setAttribute('style', 'display: block; color: green; text-align: center;');
+        setTimeout(() => {
+            window.location.href = '/login.html'
+        }, 4000);
+    } else {
+        incorreto.setAttribute('style', 'display: block; color: red; text-align: center;');
+    }
+}
